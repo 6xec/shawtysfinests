10524026f4725efdab2356c0c097ae6bca14ee49
pascal <pascaldo.pd@gmail.com>
2024-03-03 19:14:42 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `shawtysfinests`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
